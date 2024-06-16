0e0780758c5a42d6359544c09f975d843948f033
Bhushan Savale <166476482+savalebhushan@users.noreply.github.com>
2024-06-16 09:17:42 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -190,7 +190,6 @@ For countries where the top amount spent is shared, provide all customers who sp
 /* Steps to Solve:  Similar to the above question. There are two parts in question- 
 first find the most spent on music for each country and second filter the data for respective customers. */
 
-/* Method 1: using CTE */
 
 WITH Customter_with_country AS (
 		SELECT customer.customer_id,first_name,last_name,billing_country,SUM(total) AS total_spending,
@@ -202,29 +201,3 @@ WITH Customter_with_country AS (
 SELECT * FROM Customter_with_country WHERE RowNo <= 1
 
 
-/* Method 2: Using Recursive */
-
-WITH RECURSIVE 
-	customter_with_country AS (
-		SELECT customer.customer_id,first_name,last_name,billing_country,SUM(total) AS total_spending
-		FROM invoice
-		JOIN customer ON customer.customer_id = invoice.customer_id
-		GROUP BY 1,2,3,4
-		ORDER BY 2,3 DESC),
-
-	country_max_spending AS(
-		SELECT billing_country,MAX(total_spending) AS max_spending
-		FROM customter_with_country
-		GROUP BY billing_country)
-
-SELECT cc.billing_country, cc.total_spending, cc.first_name, cc.last_name, cc.customer_id
-FROM customter_with_country cc
-JOIN country_max_spending ms
-ON cc.billing_country = ms.billing_country
-WHERE cc.total_spending = ms.max_spending
-ORDER BY 1;
-
-
-/* source: www.youtube.com/@RishabhMishraOfficial */
-
-/* Thank You :) */
